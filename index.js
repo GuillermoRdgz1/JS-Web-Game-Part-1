af3d233cf4c371a0b6f404bfb90bdb5213a5282d
guillermo rodriguez <guillermordgz.96@gmail.com>
2022-11-15 18:46:54 -0800

    add functions

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage (url, left, bottom) {
+    let Object = document.createElement('img')
+    Object.src = url
+    Object.style.position = 'fixed'
+    Object.style.left = left + 'px'
+    Object.style.bottom = bottom + 'px'
+    document.body.append(Object)
+    return Object
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+}
