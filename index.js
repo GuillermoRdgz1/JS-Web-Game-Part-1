4c665f17baab307cd6a592e346e2d34bc9c2539c
guillermo rodriguez <guillermordgz.96@gmail.com>
2022-11-15 18:59:25 -0800

    add items

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function newItem(url, left, bottom){
         object.remove()
     })
 }
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
